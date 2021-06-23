4656f15d619f5dd806e0aff01f6bc82acb8406eb
ExploreNcrack <zhaozhen@ualberta.ca>
2021-06-23 15:28:22 -0600

    add smooth scroll behaviour back

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -17,7 +17,7 @@ const Messages = (props) => {
   const messagesEndRef = useRef(null);
 
   const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView();
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(() => {
